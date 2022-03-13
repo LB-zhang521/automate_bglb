b710d805b4611561d5920e14f2e8d1fe9d39e945
bglb <bglb@qq.com>
2022-03-14 02:19:02 +0800

    fix: add pyenv.sh

--- sh/build_version.sh
+++ sh/build_version.sh
@@ -1,52 +1,47 @@
-###
- # @Author: bglb
- # @Email: bglb@qq.com
- # @Date: 2021-12-10 21:35:43
- # @LastEditTime: 2021-12-10 21:35:55
- # @Description: build_version
-### 
 #!/bin/bash
+###
+# @Author: bglb
+# @Email: bglb@qq.com
+# @Date: 2021-12-10 21:35:43
+# @LastEditTime: 2021-12-10 21:35:55
+# @Description: build_version
+###
 
-# 版本增加函数 
-increment_versiovn ()
-{
-  declare -a part=( ${1//\./ } )
-  declare    new
-  declare -i carry=1
+# 版本增加函数
+increment_versiovn() {
+	declare -a part=(${1//\./ })
+	declare new
+	declare -i carry=1
 
-  for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 ));
-  do
-       len=${#part[CNTR]}
-       new=$((part[CNTR]+carry))
-       [ ${#new} -gt $len ] && carry=1 || carry=0
-       [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
-  done
-  new="${part[*]}"
-  echo "${new// /.}"
+	for ((CNTR = ${#part[@]} - 1; CNTR >= 0; CNTR -= 1)); do
+		len=${#part[CNTR]}
+		new=$((part[CNTR] + carry))
+		[ ${#new} -gt $len ] && carry=1 || carry=0
+		[ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
+	done
+	new="${part[*]}"
+	echo "${new// /.}"
 }
 # 判断 当前所在分支
-#!/bin/bash
-if branch=$(git symbolic-ref --short -q HEAD)
-then
-	if [ "$branch" = "master" ]
-	then
+if branch=$(git symbolic-ref --short -q HEAD); then
+	if [ "$branch" = "master" ]; then
 		echo master branch is not allow build version
 	else
 		# read old version
-       		version=$(cat ./version)
+		version=$(cat ./version)
 
-        	# clear version
-        	cat /dev/null > ./version
+		# clear version
+		cat /dev/null >./version
 
-        	# write new version
-        	echo $(increment_versiovn $version) > ./version
+		# write new version
+		echo $(increment_versiovn $version) >./version
 
-        	# replace file's "\r" to ""
-        	sed -i "s/$/\r/g" ./version
+		# replace file's "\r" to ""
+		sed -i "s/$/\r/g" ./version
 
-        	git add ./version
-        	git commit ./version -m "update version: $(increment_versiovn $version)"
-        	git push
-        	echo "build version and push successfully form $branch"
+		git add ./version
+		git commit ./version -m "update version: $(increment_versiovn $version)"
+		git push
+		echo "build version and push successfully form $branch"
 	fi
 fi
---
+++ sh/pyenv.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+# python3 env manager
+# python3 虚拟环境管理
+
+# 默认家目录/PythonEnv 为虚拟环境根目录
+action=$1
+defaltAction=(create remove list use)
+envName=$2
+tempPyEnvPath=$PythonEnvRoot/$envName
+
+Usage() {
+    cat <<EOF
+
+Usage:
+    pyenv action [envName]
+
+        * create envName -- 在 $PythonEnvRoot/{envName} 目录下 创建python虚拟环境
+                         
+        * remove envName -- 删除某个虚拟环境
+       
+        * use envName -- 返回激活某个虚拟环境的shell命令
+
+        * exit|deactivate envName -- 退出某个虚拟环境
+
+        * list -- 显示所有虚拟环境
+    
+    \$(pyenv use envName)
+        使用某个虚拟环境
+
+    pyenv -h      (显示帮助)
+
+EOF
+    exit 1
+}
+
+checkParams() {
+    case $action in
+    create)
+        # 判断是否输入 envName
+        if test -z $envName; then
+            echo "虚拟环境名称不能为空"
+            exit
+        fi
+
+        # 判断 envName 是否包含 $PythonEnvRoot
+        if [[ $envName =~ "/" ]]; then
+            echo "虚拟环境名称不能包含 '/' "
+            exit
+        fi
+        if [ -d $tempPyEnvPath ]; then
+            echo "虚拟环境 $envName 已经存在！请先删除！"
+            exit
+        fi
+        create
+        ;;
+
+    remove)
+        remove
+        ;;
+
+    list)
+        list
+        ;;
+    use)
+        if test -z $envName; then
+            echo "虚拟环境名称不能为空"
+            exit
+        fi
+        if [ ! -d $tempPyEnvPath ]; then
+            echo "虚拟环境 $tempPyEnvPath 不存在"
+        else
+            use
+        fi
+        ;;
+
+    -h)
+        Usage
+        ;;
+    *)
+        Usage
+        ;;
+    esac
+}
+
+# 创建 env 环境
+create() {
+    echo "$envName 虚拟环境开始创建......"
+    if [ ! -d $PythonEnvRoot ]; then
+        mkdir $PythonEnvRoot
+    fi
+    # python3 -m venv $tempPyEnvPath
+    if [ $? != 0 ]; then
+        echo "创建虚拟环境 $envName 失败!"
+        exit
+    else
+        echo "创建虚拟环境 $envName 成功!"
+    fi
+}
+
+remove() {
+    if [ ! -d $tempPyEnvPath ]; then
+        echo "虚拟环境 $envName 不存在"
+    else
+        rm -rf $tempPyEnvPath
+        echo "删除虚拟环境 $envName 成功"
+    fi
+}
+
+list() {
+    #  ls -F | grep '/$'
+    echo "虚拟环境根目录: $PythonEnvRoot"
+    for line in $(ls -F $PythonEnvRoot | grep '/$'); do
+        echo "* (${line:0:${#str}-1}) -- $PythonEnvRoot/$line"
+    done
+}
+
+use() {
+    echo "source $tempPyEnvPath/bin/activate"
+}
+checkParams
